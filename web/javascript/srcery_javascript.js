e23a10ab4fcbee26e802a0f87eb73239d60503a2
Birger J. Nordølum <birgerjn@gmail.com>
2018-06-29 10:38:13 +0200

    Added JavaScript palette

---
+++ web/javascript/srcery_javascript.js
@@ -0,0 +1,33 @@
+// srcery Color Scheme - JavaScript
+
+// Color Palette {{{
+
+let colors = {
+  'black': '#1C1B19',
+  'red': '#EF2F27',
+  'green': '#519F50',
+  'yellow': '#FBB829',
+  'blue': '#2C78BF',
+  'magenta': '#E02C6D',
+  'cyan': '#0AAEB3',
+  'white': '#918175',
+  'brightblack': '#2D2C29',
+  'brightred': '#F75341',
+  'brightgreen': '#98BC37',
+  'brightyellow': '#FED06E',
+  'brightblue': '#68A8E4',
+  'brightmagenta': '#FF5C8F',
+  'brightcyan': '#53FDE9',
+  'brightwhite': '#FCE8C3',
+
+  'orange': '#D75F00',
+  'brorange': '#FF8700',
+  'hard_black': '#121212',
+  'xgray1': '#262626',
+  'xgray2': '#303030',
+  'xgray3': '#3A3A3A',
+  'xgray4': '#444444',
+  'xgray5': '#4E4E4E'
+}
+
+// }}}
