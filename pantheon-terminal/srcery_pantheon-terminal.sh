a597e242edb792aa9fb8a20160a242c0ae64d1d3
Daniel Berg <mail@roosta.sh>
2020-01-11 15:17:57 +0100

    Rebuild configs

--- pantheon-terminal/srcery_pantheon-terminal.sh
+++ pantheon-terminal/srcery_pantheon-terminal.sh
@@ -5,5 +5,5 @@ name='srcery'
 cursor-color='#fbb829'
 foreground='#fce8c3'
 background='#1c1b19'
-palette='#1c1b19:#ef2f27:#519f50:#fbb829:#2c78bf:#e02c6d:#0aaeb3:#918175:#2d2c29:#f75341:#98bc37:#fed06e:#68a8e4:#ff5c8f:#53fde9:#fce8c3'
+palette='#1c1b19:#ef2f27:#519f50:#fbb829:#2c78bf:#e02c6d:#0aaeb3:#b6a88d:#918175:#f75341:#98bc37:#fed06e:#68a8e4:#ff5c8f:#53fde9:#fce8c3'
 COLORS
