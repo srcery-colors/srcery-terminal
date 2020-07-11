0b70fe7e79221092fb35566f0ac90c6bbea4c441
Birger J. Nordølum <contact@mindtooth.no>
2020-07-12 00:36:44 +0200

    feat: add color output

--- go-palette-previewer/main.go
+++ go-palette-previewer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	. "github.com/logrusorgru/aurora"
@@ -10,7 +11,7 @@ type Color struct {
 	Name string
 	Term uint8
 	Hex  string
-	RGB  string
+	RGB  RGB
 }
 
 type RGB struct {
@@ -19,36 +20,49 @@ type RGB struct {
 	blue  int
 }
 
+func (v *RGB) ToString() string {
+	return fmt.Sprintf("%d, %d, %d", v.red, v.green, v.blue)
+}
+
 func main() {
+	verbose := flag.Bool("c", false, "Print color tags.")
+	flag.Parse()
+
 	var colors []Color
 
-	colors = append(colors, Color{Name: "black", Term: 0, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "red", Term: 1, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "green", Term: 2, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "yellow", Term: 3, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "blue", Term: 4, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "magenta", Term: 5, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "cyan", Term: 6, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "white", Term: 7, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "bright_black", Term: 8, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "bright_red", Term: 9, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "bright_green", Term: 10, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "bright_yellow", Term: 11, Hex: "#0c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "bright_blue", Term: 12, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "bright_magenta", Term: 13, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "bright_cyan", Term: 14, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "bright_white", Term: 15, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "orange", Term: 166, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "bright_orange", Term: 208, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "hard_black", Term: 233, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "xgray1", Term: 235, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "xgray2", Term: 236, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "xgray3", Term: 237, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "xgray4", Term: 238, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "xgray5", Term: 239, Hex: "#1c1B19", RGB: "28, 27, 25"})
-	colors = append(colors, Color{Name: "xgray6", Term: 240, Hex: "#1c1B19", RGB: "28, 27, 25"})
+	colors = append(colors, Color{Name: "black", Term: 0, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "red", Term: 1, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "green", Term: 2, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "yellow", Term: 3, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "blue", Term: 4, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "magenta", Term: 5, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "cyan", Term: 6, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "white", Term: 7, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "bright_black", Term: 8, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "bright_red", Term: 9, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "bright_green", Term: 10, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "bright_yellow", Term: 11, Hex: "#0c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "bright_blue", Term: 12, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "bright_magenta", Term: 13, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "bright_cyan", Term: 14, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "bright_white", Term: 15, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "orange", Term: 166, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "bright_orange", Term: 208, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "hard_black", Term: 233, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "xgray1", Term: 235, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "xgray2", Term: 236, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "xgray3", Term: 237, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "xgray4", Term: 238, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "xgray5", Term: 239, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
+	colors = append(colors, Color{Name: "xgray6", Term: 240, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
 
 	for _, c := range colors {
-		fmt.Printf("%s (%s)\n", Index(c.Term, "█████████"), c.Name)
+		fmt.Printf("%s | %s", Index(c.Term, "██████████"), c.Name)
+
+		if *verbose {
+			fmt.Printf("\n%d, %s, (%s)", c.Term, c.Hex, c.RGB.ToString())
+		}
+
+		fmt.Println()
 	}
 }
