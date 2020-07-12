138b412606778c926a3f581e09d42b2446585d93
Birger J. Nordølum <contact@mindtooth.no>
2020-07-12 02:07:52 +0200

    feat: read from json

--- go-palette-previewer/main.go
+++ go-palette-previewer/main.go
@@ -1,63 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 
-	. "github.com/logrusorgru/aurora"
+	"github.com/logrusorgru/aurora"
 )
 
+var au aurora.Aurora
+
+var verbose = flag.Bool("c", false, "Print color tags")
+var toJson = flag.Bool("json", false, "Output to a JSON file")
+var srcery Srcery
+
+type Srcery struct {
+	Colors []Color `json:"colors"`
+}
+
 type Color struct {
-	Name string
-	Term uint8
-	Hex  string
-	RGB  RGB
+	Name string `json:"name"`
+	Term uint8  `json:"term"`
+	Hex  string `json:"hex"`
+	RGB  RGB    `json:"rgb"`
 }
 
 type RGB struct {
-	red   int
-	green int
-	blue  int
+	Red   int `json:"red"`
+	Green int `json:"green"`
+	Blue  int `json:"blue"`
 }
 
 func (v *RGB) ToString() string {
-	return fmt.Sprintf("%d, %d, %d", v.red, v.green, v.blue)
+	return fmt.Sprintf("%d, %d, %d", v.Red, v.Green, v.Blue)
+}
+
+func init() {
+	flag.Parse()
+	au = aurora.NewAurora(true)
+
+	// Import colors
+	file, _ := os.OpenFile("colors.json", os.O_RDONLY, os.ModePerm)
+	defer file.Close()
+
+	dec := json.NewDecoder(file)
+
+	if err := dec.Decode(&srcery); err != nil {
+		log.Println(err)
+	}
 }
 
 func main() {
-	verbose := flag.Bool("c", false, "Print color tags.")
-	flag.Parse()
-
-	var colors []Color
-
-	colors = append(colors, Color{Name: "black", Term: 0, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "red", Term: 1, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "green", Term: 2, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "yellow", Term: 3, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "blue", Term: 4, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "magenta", Term: 5, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "cyan", Term: 6, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "white", Term: 7, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "bright_black", Term: 8, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "bright_red", Term: 9, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "bright_green", Term: 10, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "bright_yellow", Term: 11, Hex: "#0c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "bright_blue", Term: 12, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "bright_magenta", Term: 13, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "bright_cyan", Term: 14, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "bright_white", Term: 15, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "orange", Term: 166, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "bright_orange", Term: 208, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "hard_black", Term: 233, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "xgray1", Term: 235, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "xgray2", Term: 236, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "xgray3", Term: 237, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "xgray4", Term: 238, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "xgray5", Term: 239, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-	colors = append(colors, Color{Name: "xgray6", Term: 240, Hex: "#1c1B19", RGB: RGB{28, 27, 25}})
-
-	for _, c := range colors {
-		fmt.Printf("%s | %s", Index(c.Term, "██████████"), c.Name)
+	for _, c := range srcery.Colors {
+		fmt.Printf("%s | %s", au.Index(c.Term, "██████████"), c.Name)
 
 		if *verbose {
 			fmt.Printf("\n%d, %s, (%s)", c.Term, c.Hex, c.RGB.ToString())
