8724579bcd593620ab4f2f09a3947ac8f3915a87
planet36 <planet36@users.noreply.github.com>
2021-02-12 18:20:34 -0500

    Rebuild config

--- st/srcery_st.h
+++ st/srcery_st.h
@@ -1,43 +1,38 @@
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+	/* 8 normal colors */
+	"#1c1b19",
+	"#ef2f27",
+	"#519f50",
+	"#fbb829",
+	"#2c78bf",
+	"#e02c6d",
+	"#0aaeb3",
+	"#d0bfa1",
 
-  /* 8 normal colors */
-  [0] = "#1c1b19", /* black   */
-  [1] = "#ef2f27", /* red     */
-  [2] = "#519f50", /* green   */
-  [3] = "#fbb829", /* yellow  */
-  [4] = "#2c78bf", /* blue    */
-  [5] = "#e02c6d", /* magenta */
-  [6] = "#0aaeb3", /* cyan    */
-  [7] = "#d0bfa1", /* white   */
+	/* 8 bright colors */
+	"#918175",
+	"#f75341",
+	"#98bc37",
+	"#fed06e",
+	"#68a8e4",
+	"#ff5c8f",
+	"#53fde9",
+	"#fce8c3",
 
-  /* 8 bright colors */
-  [8]  = "#918175", /* black   */
-  [9]  = "#f75341", /* red     */
-  [10] = "#98bc37", /* green   */
-  [11] = "#fed06e", /* yellow  */
-  [12] = "#68a8e4", /* blue    */
-  [13] = "#ff5c8f", /* magenta */
-  [14] = "#53fde9", /* cyan    */
-  [15] = "#fce8c3", /* white   */
+	[255] = 0,
 
-  /* special colors */
-  [256] = "#1c1b19", /* background */
-  [257] = "#fce8c3", /* foreground */
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#fce8c3", /* foreground */
+	"#1c1b19", /* background */
+	"#fbb829", /* cursor */
 };
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor
+ * foreground, background, cursor, reverse cursor
  */
-static unsigned int defaultfg = 257;
-static unsigned int defaultbg = 256;
-static unsigned int defaultcs = 3;
-
-/*
- * Colors used, when the specific fg == defaultfg. So in reverse mode this
- * will reverse too. Another logic would only make the simple feature too
- * complex.
- */
-static unsigned int defaultitalic = 7;
-static unsigned int defaultunderline = 7;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 257;
